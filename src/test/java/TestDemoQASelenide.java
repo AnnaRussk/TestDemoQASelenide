8bc76f4775be3d40a71ce0a33d0eddc3d413c3ef
@russkikh_anna <anna.russkih13@gmail.com>
2025-02-04 13:14:39 +0700

    All tests done

--- src/test/java/TestDemoQASelenide.java
+++ src/test/java/TestDemoQASelenide.java
@@ -24,4 +24,35 @@ public class TestDemoQASelenide {
         $x("//button[@id='submit']").click();
         $x("//p[@id='name']").shouldHave(text("Name:Какое-то Имя"));
     }
+
+    @Test
+    public void checkBoxTest() {
+        open(url);
+        $x("//h5[contains(text(), 'Elements')]").scrollTo().click();
+        $x("//span[contains(text(), 'Check Box')]").click();
+        $x("//span[@class='rct-checkbox']").click();
+        $x("//span[contains(text(), 'You have selected')]").shouldHave(text("You have selected :"));
+    }
+
+    @Test
+    public void radioButtonTest() {
+        open(url);
+        $x("//h5[contains(text(), 'Elements')]").scrollTo().click();
+        $x("//span[contains(text(), 'Radio Button')]").click();
+        $x("//label[@class='custom-control-label']").click();
+        $x("//span[@class='text-success']").shouldHave(text("Yes"));
+    }
+
+    @Test
+    public void formText() {
+        open(url);
+        $x("//h5[contains(text(), 'Forms')]").click();
+        $x("//span[contains(text(), 'Practice Form')]").click();
+        $x("//input[@id='firstName']").sendKeys("Имя");
+        $x("//input[@id='lastName']").sendKeys("Фамилия");
+        $x("//div[@class='custom-control custom-radio custom-control-inline']").click();
+        $x("//input[@id='userNumber']").sendKeys("1234567890");
+        $x("//button[@id='submit']").scrollTo().click();
+        $x("//div[contains(text(), 'Thanks for submitting the form')]").shouldHave(text("Thanks for submitting the form"));
+    }
 }
